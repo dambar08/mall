4c001c60cfc6dfe42267ba317a2d96940b4332c7
Dambar <notdambar@gmail.com>
2026-07-13 07:23:24 +0000

    add: hosts support for github codespace

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,6 +26,16 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
+   config.jwt do |jwt|
+    jwt.secret = ENV.fetch("DEVISE_JWT_SECRET_KEY")
+    jwt.dispatch_requests = [
+      ["POST", %r{^/api/v1/auth/sign_in$}]
+    ]
+    jwt.revocation_requests = [
+      ["DELETE", %r{^/api/v1/auth/sign_out$}]
+    ]
+    jwt.expiration_time = 24.hours.to_i
+  end
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
